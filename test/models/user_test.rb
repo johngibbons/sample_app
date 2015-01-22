fd6222b761bab96654d7fa969e8fa227c852590f
John Gibbons <johngibbons10@gmail.com>
2015-01-22 12:12:54 -0700

    add validation to users name and email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,59 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup 
+    @user = User.new(name: "Example User", email: "email@example.com")
+  end
+
+  test "User should be valid" do
+    assert @user.valid?
+  end
+
+  test "User name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "User email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "User name should be 50 characters or less" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "User email should be 255 characters or less" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "Valid email addresses should pass" do
+    valid_emails = %w[sample@example.com sample@example.COM sam_Ple@example.org 
+      sAm.ple@example.net foo.bar@sample.jp foo+bar@foo.bar.org]
+
+    valid_emails.each do |email|
+      @user.email = email
+      assert @user.valid?, "#{email.inspect} should be valid"
+    end
+  end
+
+  test "Invalid email addresses should not pass" do
+    invalid_emails = %w[sample@example,org sample_at_foo.com user.name@example. 
+      foo@bar_baz.com foo@bar+baz.com]
+
+    invalid_emails.each do |invalid_email|
+      @user.email = invalid_email
+      assert_not @user.valid?, "#{invalid_email.inspect} should not be valid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
 end
