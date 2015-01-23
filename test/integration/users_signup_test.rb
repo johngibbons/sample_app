a1e040f75704fc418760d6f772e79f7a6bb6c84e
John Gibbons <johngibbons10@gmail.com>
2015-01-22 21:18:58 -0700

    ch 7 exercises complete

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,6 +12,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                 password_confirmation:  "bar" }
     end
     assert_template "users/new"
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
   end
 
   test "valid signup creates user" do
@@ -24,5 +26,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                 password_confirmation:  "foobar" }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
